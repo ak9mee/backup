a05fb720202162c5233d94435d89236aad1f9b50
jonathan <112445400+ak9mee@users.noreply.github.com>
2023-02-03 13:53:16 -0600

    Create subtitle.js

---
+++ lioxryt/js/subtitle.js
@@ -0,0 +1,90 @@
+// script to generate random greetings
+
+function changeSub(num) {
+  document.getElementById("subtitle").innerHTML = say[num];
+}
+
+var say = [
+  "The tech teams worst nightmare.",
+  "Better than coolmathgames",
+  "HEY! is this thing on",
+  "Don't get caught",
+  "There is definitely no games here",
+  "ONG",
+  "🤨",
+  "This project was made possible due to extensive use of narcotics",
+  "Last chance to look at me Hector",
+  "👋hello👋 and welcome to the Los Pollos hermanos 👪family👪👪 my name📛 is Gustavo but you can 📲call 📲 me Gus",
+  "According to all known laws of aviation,  : there is no way a bee should be able to fly.",
+  "amojngus ahdahahahayah i scab cab  cab can spell correctky ahahahha your bad and im gododd",
+  "no school, no need to block this",
+  "I made it mom! I'm on a randomized subtitle!",
+  "Playing games in my class, ay?",
+  "A moment of silence for adobe flash player",
+  "Whats my name? You're Heisenberg. You're God Damn Right",
+  "Anyone remember when the background was white? No? Just me?",
+  "If you are over 25 and own a computer, you wont last 5 seconds playing these games",
+  "only in ohio 💀",
+  "The Rizzard of Oz",
+  "We have a <a href='https://discord.com/invite/Qf5wmbGzk9'>Discord</a> Now",
+  "the god made this site",
+  "good soup 🍲",
+  "You wouldn't download a car?",
+  "L for Coolmath. This site is better still.",
+  "Jessie you were supposed to get a plastic container",
+  "N Gon go hard on this site",
+  "This is just schoology ok? idk or what i think this is a glitch",
+  "oooohhhh mario's tunnel of doom very scary",
+  "goofy ahh no admins",
+  "(Hello this is the principal of Elmer High, please stop playing games and take down this site.)-Some Bum Loser Probably",
+  "We should re-name the website to yourmom.com",
+  "Ur moms favorite ",
+  "I'm Gustavo But you can call me Gus",
+  "Walter Black",
+  "Does a set of all sets contain itself?",
+  " i died on my 12 year hardcore minecraft world  :(",
+  "Ngl CFISD Goofy ASF",
+  "the fitnessgram pacer test is a multistage aerobic capacity test",
+  "i know for a fact that you have a 60 in math and science and you playin games you bozo",
+  "Do your balls itch?",
+  "These aren't the games you're looking for",
+  "Nefarious Darius",
+  "numnut",
+  "👹I like to creep around my home and act like a goblin👹",
+  "Among Us VR?",
+  "If you ain’t in the <a href='https://discord.com/invite/Qf5wmbGzk9'>discord server</a> then where the hell you atttt", 
+  "<img src='/img/bingchilling.jpg'/>",
+  "i lost my v card two days ago thanks to this site",
+  "error = ran out of subtitles",
+  "your either a smart fella or a fart smella",
+  "Welcome to your final test, I’m Mr. Beast",
+  "better call saul",
+  "internet admin thinks they can stop us",
+  "Schoology is Down",
+  "I used this while trying to ignore the blood on the floor in school.",
+  "Where is the download ram button?",
+  "you know Pizza Hut, Just Do It",
+  "the last one to leave this website wins $100,000 - mr beast",
+  "Jesse, we need to game Jesse.",
+  "metro boomin make it boom ",
+  'as  the big man said "cock and balls"',
+  "what rhymes with digger",
+  "4 big guys:)",
+  "i ate the f***ing wii",
+  "it's really your own fault if you get caught",
+  "We are Number One",
+  "Abana",
+];
+// pick a random greeting
+var howmany = say.length;
+var bRand = 0;
+bRand = Math.random();
+bRand = Math.floor(bRand * howmany);
+sayWhat = say[bRand];
+document.getElementById("subtitle").innerHTML = sayWhat;
+
+function changeSplash(num) {
+  document.getElementById("subtitle").innerHTML = say[num];
+  var ret = "Set current splash to splash " + num + ", " + say[num];
+  return ret;
+}
